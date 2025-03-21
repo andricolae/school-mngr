0ec2cf6257558fc87a00d7a1749bd0c6a8ec3ac4
Andrei Nicolae <andreicalutiu@gmail.com>
2025-03-21 21:46:00 +0200

    updated component structure and admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,6 @@ import { RegisterComponent } from './auth/register/register.component';
 import { AdminDashComponent } from './dashboard/admin-dash/admin-dash.component';
 import { TeacherDashComponent } from './dashboard/teacher-dash/teacher-dash.component';
 import { StudentDashComponent } from './dashboard/student-dash/student-dash.component';
-import { CourseMgmtComponent } from './courses/course-mgmt/course-mgmt.component';
-import { UserMgmtComponent } from './users/user-mgmt/user-mgmt.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -13,7 +11,5 @@ export const routes: Routes = [
   { path: 'admin/dashboard', component: AdminDashComponent },
   { path: 'teacher/dashboard', component: TeacherDashComponent },
   { path: 'student/dashboard', component: StudentDashComponent },
-  { path: 'admin/courses', component: CourseMgmtComponent },
-  { path: 'admin/users', component: UserMgmtComponent },
   { path: '**', redirectTo: '/login' }
 ];
