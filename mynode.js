ca3323d80968e3249c23a7422f53b1b61afa86d6
Andrei Nicolae <andreicalutiu@gmail.com>
2025-05-24 19:20:47 +0300

    env

---
+++ mynode.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const successColor = "\x1b[32m%s\x1b[0m";
+const checkSign = "\u{2705}";
+const dotenv = require("dotenv").config({ path: ".env" });
+
+const envFile = `export const firebaseConfig = {
+    apiKey: '${process.env.API_KEY}',
+    authDomain: '${process.env.AUTH_DOM}',
+    projectId: '${process.env.PROJ_ID}',
+    storageBucket: '${process.env.STORAGE}',
+    messagingSenderId: '${process.env.MESS_SEND_ID}',
+    appId: '${process.env.APP_ID}',
+    measurementId: '${process.env.MEASURE_ID}',
+};
+`;
+const targetPath = path.join(__dirname, "./environment.ts");
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.ts`
+    );
+  }
+});
